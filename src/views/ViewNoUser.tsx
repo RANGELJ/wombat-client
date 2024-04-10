37c1c393a9d6c8ed68a65fbde4009de810f3498e
Jorge Rangel <jorgerangel@192.168.1.3>
2024-04-10 07:44:50 -0600

    Can login with google

--- src/views/ViewNoUser.tsx
+++ src/views/ViewNoUser.tsx
@@ -1,5 +1,7 @@
 import { Navigate, useOutletContext } from 'react-router-dom'
 import ScreenCardWithTitle from '../components/ScreenCardWithTitle'
+import { GoogleAuthProvider, getAuth, signInWithRedirect } from 'firebase/auth'
+import firebaseGetApp from '../shared/firebaseGetApp'
 
 export const Component = () => {
   const user = useOutletContext()
@@ -11,7 +13,14 @@ export const Component = () => {
   return (
     <ScreenCardWithTitle title="Welcome!" color="primary">
       <button
-        className="bg-primary-700 text-white p-3 rounded-lg transition-colors"
+        className="bg-primary-700 active:bg-primary-800 text-white p-3 rounded-lg transition-colors"
+        type="button"
+        onClick={() => {
+          const firebaseApp = firebaseGetApp()
+          const auth = getAuth(firebaseApp)
+          const provider = new GoogleAuthProvider()
+          signInWithRedirect(auth, provider)
+        }}
       >
         Login with Google
       </button>
