f3eeb483ac0b1e7b8e3593130ea0d51a1e27ce13
Jorge Rangel <jorgerangel@192.168.1.3>
2024-04-10 07:04:28 -0600

    Basic error boundary setted up

--- src/views/ViewRoot.tsx
+++ src/views/ViewRoot.tsx
@@ -26,8 +26,13 @@ export const ErrorBoundary = () => {
   const error = routeError instanceof Error ? routeError : new Error('Unknown error')
 
   return (
-    <div>
-      {error.message}
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="bg-white w-3/4 md:w-1/3 h-3/4 md:h-1/3 shadow-md border-t-2 border-red-400 flex flex-col p-2">
+        <h1 className="text-2xl text-center">Error</h1>
+        <div className="grow flex justify-center items-center">
+          <p className="text-sm">{error.message}</p>
+        </div>
+      </div>
     </div>
   )
 }
